b5136b3a90ddb80b38258473c6a2709c5362408b
DerekYangYC <info.yyc@gmail.com>
2014-05-08 08:38:48 +0800

    add user

--- quotation/views.py
+++ quotation/views.py
@@ -1,6 +1,9 @@
+from django.contrib import auth
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 
 
 def index(request):
@@ -13,10 +16,20 @@ def register_form(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        user = User.objects.create_user(username=form['username'], password=form['password'],
-                                        email=form['email'])
+        # form = UserCreationForm(request.POST)
+        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'],
+                                        email=request.POST['email'])
         user.save()
-        
+
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        auth.login(request, user)
+
+        return render_to_response('quotation/index.html', context_instance=RequestContext(request))
+
+
+def logout(request):
+    if request.user.is_authenticated():
+        auth.logout(request)
         return render_to_response('quotation/index.html')
+
 
