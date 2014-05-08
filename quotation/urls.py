507b1b2c56efd154566bfd7f5966ee98b0045ff0
Derek Yang <derek.yang@cimb.com>
2014-05-08 17:36:26 +0800

    add login

--- quotation/urls.py
+++ quotation/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import patterns, url
 from django.conf.urls.static import static
 from quotation import views
 
-
+#$ should be used to avoid the behavior under the same parent path. Like login/ and login/submit/
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'register/form/', views.register_form,name='register_form'),
-    url(r'register/', views.register, name='register'),
-    url(r'logout/', views.logout, name='logout')
+    url(r'register/submit/$', views.register_submit, name='register_submit'),
+    url(r'register/$', views.register, name='register'),
+    url(r'logout/$', views.logout, name='logout'),
+    url(r'login/$', views.login, name='login'),
+    url(r'login/submit/$', views.login_submit, name='login_submit'),
 )+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- quotation/views.py
+++ quotation/views.py
@@ -2,19 +2,19 @@ from django.contrib import auth
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from django.template import RequestContext
 
 
 def index(request):
-    return render(request, 'quotation/index.html')
-
-
-def register_form(request):
-    return render(request, 'quotation/register.html')
+    return render_to_response(request, 'quotation/index.html')
 
 
 def register(request):
+    return render_to_response(request, 'quotation/register.html')
+
+
+def register_submit(request):
     if request.method == 'POST':
         # form = UserCreationForm(request.POST)
         user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'],
@@ -24,12 +24,33 @@ def register(request):
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
         auth.login(request, user)
 
-        return render_to_response('quotation/index.html', context_instance=RequestContext(request))
+        # return render_to_response('quotation/index.html', context_instance=RequestContext(request))
+        # return redirect('quotation/index.html')
+        return redirect('/quotation/')
+
+
+def login(request):
+    return render_to_response(request, 'quotation/login.html')
+
+
+def login_submit(request):
+    print("haha")
+    if request.method == 'POST':
+        print("in post")
+        print(request.POST['username'])
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        print(user)
+        print(request.POST['username'])
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return redirect('/quotation/')
+        else:
+            return redirect('/quotation/login/', RequestContext({'isLogin': '1'}))
 
 
 def logout(request):
     if request.user.is_authenticated():
         auth.logout(request)
-        return render_to_response('quotation/index.html')
-
+        # return render_to_response('quotation/index.html')
+        return redirect('/quotation/')
 
